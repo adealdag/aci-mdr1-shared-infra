9001e522c8692e99e55f6c594ceb8cc98ea5fe4c
Alejandro de Alda <adealdag@cisco.com>
2021-08-19 13:15:50 +0200

    Added default values

--- aci_access_policies_vars.tf
+++ aci_access_policies_vars.tf
@@ -1,5 +1,6 @@
 variable "leaf_switches" {
   description = "List of leaf switches part of the fabric. The key of each item is the leaf ID"
+  default = {}
   type = map(object({
     name        = string
     oob_ip_mask = string
@@ -9,6 +10,7 @@ variable "leaf_switches" {
 
 variable "spine_switches" {
   description = "List of spine switches part of the fabric. The key of each item is the leaf ID"
+  default = {}
   type = map(object({
     name        = string
     oob_ip_mask = string
@@ -17,7 +19,8 @@ variable "spine_switches" {
 }
 
 variable "vpc_domain" {
-  description = "List of Virtual Port Channels (vPC) domains in the fabric. The key of each item is the VPC ID. "
+  description = "List of Virtual Port Channels (vPC) domains in the fabric. The key of each item is the VPC ID."
+  default = {}
   type = map(object({
     left_node_id  = string
     right_node_id = string
