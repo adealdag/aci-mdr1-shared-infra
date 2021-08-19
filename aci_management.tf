8e3225b79e99e5e78edf02f590e780b0fbb423ab
Alejandro de Alda <adealdag@cisco.com>
2021-08-19 15:18:55 +0200

    First commit management config

---
+++ aci_management.tf
@@ -0,0 +1,34 @@
+# Out of Band Management
+
+resource "aci_node_mgmt_epg" "oob_mgmt_epg" {
+  type                  = "out_of_band"
+  management_profile_dn = "uni/tn-mgmt/mgmtp-default"
+  name                  = var.fabric_management_oob.epg_name
+}
+
+resource "aci_static_node_mgmt_address" "oob_ip_addr" {
+  for_each = var.fabric_nodes
+
+  management_epg_dn = aci_node_mgmt_epg.oob_mgmt_epg.id
+  t_dn              = "topology/pod-${each.value.pod_id}/node-${each.key}"
+  type              = "out_of_band"
+  addr              = each.value.oob_ip_addr
+  gw                = var.fabric_management_oob.gateway_ip
+}
+
+resource "aci_node_mgmt_epg" "inb_mgmt_epg" {
+  type                  = "in_band"
+  management_profile_dn = "uni/tn-mgmt/mgmtp-default"
+  name                  = var.fabric_management_inb.epg_name
+  encap                 = var.fabric_management_inb.vlan_encap
+}
+
+resource "aci_static_node_mgmt_address" "inb_ip_addr" {
+  for_each = { for k, v in var.fabric_nodes : k => v if v.inb_ip_addr != "" }
+
+  management_epg_dn = aci_node_mgmt_epg.inb_mgmt_epg.id
+  t_dn              = "topology/pod-${each.value.pod_id}/node-${each.key}"
+  type              = "in_band"
+  addr              = each.value.inb_ip_addr
+  gw                = var.fabric_management_inb.gateway_ip
+}
