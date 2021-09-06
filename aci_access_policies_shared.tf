67b2f720261417ddd8fd48b6c4dd5a2237877876
Alejandro de Alda <adealdag@cisco.com>
2021-09-06 16:21:06 +0200

    STP policy towards N9K changed

--- aci_access_policies_shared.tf
+++ aci_access_policies_shared.tf
@@ -18,7 +18,7 @@ resource "aci_leaf_access_port_policy_group" "core" {
   relation_infra_rs_lldp_if_pol = aci_lldp_interface_policy.on.id
   relation_infra_rs_l2_if_pol   = aci_l2_interface_policy.global_scope.id
   relation_infra_rs_mcp_if_pol  = aci_miscabling_protocol_interface_policy.on.id
-  relation_infra_rs_stp_if_pol  = aci_spanning_tree_interface_policy.filter_guard.id
+  relation_infra_rs_stp_if_pol  = aci_spanning_tree_interface_policy.default.id
 
   relation_infra_rs_att_ent_p = aci_attachable_access_entity_profile.main_aaep.id
 }
